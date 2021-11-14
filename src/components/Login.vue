7a9bcfb18a84c50aff444dea90f25e5b3086e041
chunfen <1070692044@qq.com>
2021-11-14 20:35:34 +0800

    新增路由和页面

---
+++ src/components/Login.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>
+        {{msg}}
+    </h1>
+</template>
+
+<script>
+    export default {
+      name: 'Login',
+      data() {
+        return{
+          msg: 'Login 登录注册页面'
+        }
+      }
+    }
+</script>
+
+<style scoped>
+h1{
+  color: red;
+}
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login.vue'
+import Noteboos from '@/components/NotebookList.vue'
+import Note from '@/components/NoteDetail.vue'
+import Tash from '@/components/TashDetail.vue'
 
 Vue.use(Router)
 
@@ -8,8 +11,28 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/notebooks',
+      name: 'Noteboos',
+      component: Noteboos
+    },
+    {
+      path: '/note/:noteId',
+      name: 'NoteDetail',
+      component: Note
+    },
+    {
+      path: '/trash/:noteId',
+      name: 'TashDetail',
+      component: Tash
     }
   ]
 })
