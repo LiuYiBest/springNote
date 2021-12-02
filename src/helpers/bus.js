96c8d3647c61002f48716ce9c5fdaf32107bb5b9
liuyidiao <1070692044@qq.com>
2021-12-02 22:41:19 +0800

    事件传递

---
+++ src/helpers/bus.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+
+export default new Vue()
+
+/* 使用方法
+*   import Bus form '@/helper/bus'
+*   Bus.$on('test',msg=>{
+*     console.log(msg)
+*   })
+*   Bus.$emit('test'.'hello world')
+* */
+
+
+
+
+
+
+
+
+
+
+
+
+
