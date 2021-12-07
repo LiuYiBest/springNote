f4f9c821dbbf27c7605f3c306062ab39d2207fe5
liuyidiao <1070692044@qq.com>
2021-12-07 23:14:40 +0800

    完成笔记本列表页

--- src/apis/notebooks.js
+++ src/apis/notebooks.js
@@ -1,5 +1,5 @@
 import request from "../helpers/request";
-
+import {friendlyDate} from '../helpers/util'
 // 笔记本列表的API封装
 
 const URL = {
@@ -11,9 +11,27 @@ const URL = {
 
 export default {
 
+  // getAll(){
+  //   return request(URL.GET)
+  // },
+
+  //获取笔记本列表
   getAll(){
-    return request(URL.GET)
+    return new Promise((resolve,reject)=>{
+      request(URL.GET).then(res=>{
+      //  排序笔记本  直接比较时间戳字符串
+        res.data = res.data.sort((notebook1,notebook2)=>notebook1.createdAt < notebook2.createdAt)
+        res.data.forEach(notebook=>{
+             notebook.friendlyCreatedAt = friendlyDate(notebook.createdAt)
+          })
+        console.log("获取笔记本列表",res.data)
+        resolve(res)
+      }).catch(err=>{
+        reject(err)
+      })
+    })
   },
+
   //更新笔记本的id  更新笔记本的标题    默认参数
   updateNotebook(notebookId,{title = ''}={title: ''}){
     return request(URL.UPDATE.replace(':id',notebookId),'PATCH',{title})
@@ -25,7 +43,7 @@ export default {
   },
 
   //增加笔记  需要标题字段
-  addNoteBook({title=''}={title:''}){
+  addNotebook({title=''}={title:''}){
     return request(URL.ADD ,'POST',{title})
   }
 
--- src/components/NotebookList.vue
+++ src/components/NotebookList.vue
@@ -10,9 +10,11 @@
           <router-link v-for="notebook in notebooks" to="/note/1" class="notebook">
             <div>
               <span class="iconfont icon-notebook"></span> {{notebook.title}}
+              <!--      noteCounts 笔记本计数        -->
               <span>{{notebook.noteCounts}}</span>
               <span class="action" @click.stop.prevent="onEdit(notebook)">编辑</span>
               <span class="action" @click.stop.prevent="onDelete(notebook)">删除</span>
+<!--         创建时间 多少分钟 小时 天 前创建笔记本    -->
               <span class="date">{{notebook.friendlyCreatedAt}}</span>
             </div>
           </router-link>
@@ -26,7 +28,7 @@
 <script>
 import Auth from '@/apis/auth'
 import Notebooks from '@/apis/notebooks'
-// import { friendlyDate } from '@/helpers/util'
+import { friendlyDate } from '@/helpers/util'
 
 // window.Notebooks = Notebooks
 
@@ -45,46 +47,56 @@ export default {
         }
       })
 
-    // Notebooks.getAll()
-    //   .then(res => {
-    //     this.notebooks = res.data
-    //   })
+    Notebooks.getAll()
+      .then(res => {
+        this.notebooks = res.data
+      })
   },
 
   methods: {
+    // 创建笔记本
     onCreate() {
+      //获取笔记本的标题
       let title = window.prompt('创建笔记本')
+      //判断输入是否为空
       if(title.trim() === '') {
         alert('笔记本名不能为空')
         return
       }
+      //调用新增笔记本接口
       Notebooks.addNotebook({ title })
         .then(res => {
-          console.log(res)
+          console.log("新增笔记",res)
+          //创建时间
           res.data.friendlyCreatedAt = friendlyDate(res.data.createdAt)
+          //将新建笔记本数据放到最前面
           this.notebooks.unshift(res.data)
           alert(res.msg)
         })
     },
-
+    //编辑笔记本标题
     onEdit(notebook) {
       console.log('edit...', notebook)
       let title = window.prompt('修改标题', notebook.title)
+      //一个noteBooks的id 和title对象
       Notebooks.updateNotebook(notebook.id, { title })
         .then(res => {
-          console.log(res)
+          console.log("编辑笔记本",res)
+          //修改当前标题
           notebook.title = title
           alert(res.msg)
         })
     },
-
+    //删除笔记本
     onDelete(notebook) {
       console.log('delete', notebook)
+      //用户确认是否输出  Boolean类型
       let isConfirm = window.confirm('你确定要删除吗?')
       if(isConfirm) {
         Notebooks.deleteNotebook(notebook.id)
           .then(res => {
-            console.log(res)
+            console.log("删除笔记本",res)
+            //删除数据库的内容后  把页面指定book数据的下标删除掉
             this.notebooks.splice(this.notebooks.indexOf(notebook), 1)
             alert(res.msg)
           })
