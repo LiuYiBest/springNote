0a516a0f5b735edb96f8b37d9f02d16118464a61
liuyidiao <1070692044@qq.com>
2022-01-25 23:30:20 +0800

    创建笔记本修改

--- src/components/NotebookList.vue
+++ src/components/NotebookList.vue
@@ -5,17 +5,17 @@
     </header>
     <main>
       <div class="layout">
-        <h3>笔记本列表({{notebooks.length}})</h3>
+        <h3>笔记本列表({{ notebooks.length }})</h3>
         <div class="book-list">
           <router-link v-for="notebook in notebooks" to="/note/1" class="notebook">
             <div>
-              <span class="iconfont icon-notebook"></span> {{notebook.title}}
+              <span class="iconfont icon-notebook"></span> {{ notebook.title }}
               <!--      noteCounts 笔记本计数        -->
-              <span>{{notebook.noteCounts}}</span>
+              <span>{{ notebook.noteCounts }}</span>
               <span class="action" @click.stop.prevent="onEdit(notebook)">编辑</span>
               <span class="action" @click.stop.prevent="onDelete(notebook)">删除</span>
-<!--         创建时间 多少分钟 小时 天 前创建笔记本    -->
-              <span class="date">{{notebook.friendlyCreatedAt}}</span>
+              <!--         创建时间 多少分钟 小时 天 前创建笔记本    -->
+              <span class="date">{{ notebook.friendlyCreatedAt }}</span>
             </div>
           </router-link>
         </div>
@@ -28,12 +28,12 @@
 <script>
 import Auth from '@/apis/auth'
 import Notebooks from '@/apis/notebooks'
-import { friendlyDate } from '@/helpers/util'
+import {friendlyDate} from '@/helpers/util'
 
 // window.Notebooks = Notebooks
 
 export default {
-  data () {
+  data() {
     return {
       notebooks: []
     }
@@ -42,7 +42,7 @@ export default {
   created() {
     Auth.getInfo()
       .then(res => {
-        if(!res.isLogin) {
+        if (!res.isLogin) {
           this.$router.push({path: '/login'})
         }
       })
@@ -56,46 +56,55 @@ export default {
   methods: {
     // 创建笔记本
     onCreate() {
-      //获取笔记本的标题
-      let title = window.prompt('创建笔记本')
-      //判断输入是否为空
-      if(title.trim() === '') {
-        alert('笔记本名不能为空')
-        return
-      }
-      //调用新增笔记本接口
-      Notebooks.addNotebook({ title })
-        .then(res => {
-          console.log("新增笔记",res)
-          //创建时间
-          res.data.friendlyCreatedAt = friendlyDate(res.data.createdAt)
-          //将新建笔记本数据放到最前面
-          this.notebooks.unshift(res.data)
-          alert(res.msg)
+      this.$prompt('输入新笔记本标题', '创建笔记本', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        closeOnClickModal: false,
+        inputPattern: /^.{1,30}$/,
+        inputErrorMessage: '标题不能为空，且不能超过30个字符'
+      }).then(({value}) => {
+        //调用新增笔记本接口
+        return Notebooks.addNotebook({title: value})
+      }).then(res => {
+        console.log("新增笔记", res)
+        //创建时间
+        res.data.friendlyCreatedAt = friendlyDate(res.data.createdAt)
+        //将新建笔记本数据放到最前面
+        this.notebooks.unshift(res.data)
+        this.$message({
+          type: 'success',
+          message: res.msg
+        });
+      }).catch((res) => {
+        this.$message({
+          type: 'error',
+          message: '取消输入'
         })
+      })
     },
     //编辑笔记本标题
     onEdit(notebook) {
       console.log('edit...', notebook)
       let title = window.prompt('修改标题', notebook.title)
       //一个noteBooks的id 和title对象
-      Notebooks.updateNotebook(notebook.id, { title })
+      Notebooks.updateNotebook(notebook.id, {title})
         .then(res => {
-          console.log("编辑笔记本",res)
+          console.log("编辑笔记本", res)
           //修改当前标题
           notebook.title = title
           alert(res.msg)
         })
-    },
-    //删除笔记本
+    }
+    ,
+//删除笔记本
     onDelete(notebook) {
       console.log('delete', notebook)
       //用户确认是否输出  Boolean类型
       let isConfirm = window.confirm('你确定要删除吗?')
-      if(isConfirm) {
+      if (isConfirm) {
         Notebooks.deleteNotebook(notebook.id)
           .then(res => {
-            console.log("删除笔记本",res)
+            console.log("删除笔记本", res)
             //删除数据库的内容后  把页面指定book数据的下标删除掉
             this.notebooks.splice(this.notebooks.indexOf(notebook), 1)
             alert(res.msg)
