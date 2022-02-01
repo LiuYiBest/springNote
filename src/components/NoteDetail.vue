ebed3c081f82bb7db0824e8297dcbb765473b529
liuyidiao <1070692044@qq.com>
2022-02-01 17:50:20 +0800

    笔记本详情

--- src/components/NoteDetail.vue
+++ src/components/NoteDetail.vue
@@ -1,9 +1,26 @@
 <template>
   <div id="note" class="detail">
-    <note-sidebar></note-sidebar>
-    <div id="note-detail">
-      <h1>notebookId : {{ $route.query.notebookId }}</h1>
-      <h1>noteId : {{ $route.query.noteId }}</h1>
+    <note-sidebar  @update:notes="val => notes = val"></note-sidebar>
+    <div class="note-detail">
+      <div class="note-empty" v-show="!curNote.id">请选择笔记</div>
+      <div class="note-detail-ct" v-show="curNote.id">
+        <div class="note-bar">
+          <span> 创建日期: {{curNote.createdAtFriendly}}</span>
+          <span> 更新日期: {{curNote.updatedAtFriendly}}</span>
+          <span> {{statusText}}</span>
+          <span class="iconfont icon-delete" @click="deleteNote"></span>
+          <span class="iconfont icon-fullscreen" @click="isShowPreview = !isShowPreview"></span>
+        </div>
+        <div class="note-title">
+          <input type="text" v-model:value="curNote.title" @input="updateNote" @keydown="statusText='正在输入...'" placeholder="输入标题">
+        </div>
+        <div class="editor">
+          <textarea v-show="isShowPreview"  v-model:value="curNote.content" @input="updateNote" @keydown="statusText='正在输入...'" placeholder="输入内容, 支持 markdown 语法"></textarea>
+          <div class="preview markdown-body" v-html="previewContent" v-show="!isShowPreview">
+          </div>
+        </div>
+      </div>
+
     </div>
   </div>
 
@@ -34,11 +51,14 @@ export default {
 }
 </script>
 
-<style scoped>
-#note{
+<style lang="less">
+@import url(../assets/css/note-detail.less);
+
+#note {
   display: flex;
   align-items: stretch;
-  background-color: #ffffff;
+  background-color: #fff;
   flex: 1;
 }
+
 </style>
