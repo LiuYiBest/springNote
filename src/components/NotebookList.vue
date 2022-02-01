c2a9f96d834b8e9a523be972b4c5552f6d037ab2
liuyidiao <1070692044@qq.com>
2022-02-02 01:51:58 +0800

    完善用户界面

--- src/components/NotebookList.vue
+++ src/components/NotebookList.vue
@@ -9,14 +9,14 @@
         <div class="book-list">
           <router-link v-for="notebook in notebooks" :to="`/note?notebookId=${notebook.id}`" class="notebook">
             <div>
-              <span class="iconfont icon-notebook"></span> {{notebook.title}}
+              <span class="iconfont icon-notebook"></span> {{notebook.title}} 
               <span>{{notebook.noteCounts}}</span>
-              <span class="action" @click.stop.prevent="onEdit(notebook)">编辑</span>
-              <span class="action" @click.stop.prevent="onDelete(notebook)">删除</span>
-              <span class="date">{{notebook.friendlyCreatedAt}}</span>
+              <span class="action" @click.stop.prevent="onEdit(notebook)">编辑</span>  
+              <span class="action" @click.stop.prevent="onDelete(notebook)">删除</span>  
+              <span class="date">{{notebook.createdAtFriendly}}</span>              
             </div>
-          </router-link>
-        </div>
+          </router-link>              
+        </div>       
       </div>
 
     </main>
@@ -24,8 +24,7 @@
   </div>
 </template>
 <script>
-import Auth from '@/apis/auth'
-import Notebooks from '@/apis/notebooks'
+
 import { friendlyDate } from '@/helpers/util'
 import { mapState, mapActions, mapGetters } from 'vuex'
 
@@ -52,45 +51,45 @@ export default {
       'updateNotebook',
       'deleteNotebook',
       'checkLogin'
-    ]),
+      ]),
 
     onCreate() {
       this.$prompt('输入新笔记本标题', '创建笔记本', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        inputPattern: /^.{1,30}$/,
-        inputErrorMessage: '标题不能为空，且不超过30个字符'
-      }).then(({ value }) => {
-        this.addNotebook({ title: value })
-      })
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          inputPattern: /^.{1,30}$/,
+          inputErrorMessage: '标题不能为空，且不超过30个字符'
+        }).then(({ value }) => {
+          this.addNotebook({ title: value })
+        })
     },
 
     onEdit(notebook) {
       let title = ''
       this.$prompt('输入新笔记本标题', '修改笔记本', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        inputPattern: /^.{1,30}$/,
-        inputValue: notebook.title,
-        inputErrorMessage: '标题不能为空，且不超过30个字符'
-      }).then(({ value }) => {
-        this.updateNotebook({ notebookId: notebook.id, title: value })
-      })
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          inputPattern: /^.{1,30}$/,
+          inputValue: notebook.title,
+          inputErrorMessage: '标题不能为空，且不超过30个字符'
+        }).then(({ value }) => {
+          this.updateNotebook({ notebookId: notebook.id, title: value })
+        })
     },
 
     onDelete(notebook) {
       this.$confirm('确认要删除笔记本吗', '删除笔记本', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
-      }).then(() => {
-        this.deleteNotebook({ notebookId: notebook.id })
-      })
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.deleteNotebook({ notebookId: notebook.id })
+        })
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
-@import url(../assets/css/notebook-list.less);
+  @import url(../assets/css/notebook-list.less);
 </style>
