591e9fd50619828e84ed6db9e0a6518cf4d0a2b2
liuyidiao <1070692044@qq.com>
2022-02-02 01:50:08 +0800

    完善用户界面

--- src/apis/auth.js
+++ src/apis/auth.js
@@ -1,29 +1,26 @@
-import request from "../helpers/request";
+import request from '@/helpers/request'
 
-//接口路径
-const  URL = {
-  REGISTER: '/auth/register',       //注册
-  LOGIN: '/auth/login',             //登录
-  LOGOUT: '/auth/logout',           //退出
-  GET_INFO: '/auth',                //登录状态
+const URL = {
+  REGISTER: '/auth/register',
+  LOGIN: '/auth/login',
+  LOGOUT: '/auth/logout',
+  GET_INFO: '/auth'
 }
 
-//导出接口
 export default {
-  //注册方法
-  register({username, password}){
-    return request(URL.REGISTER,'POST',{username,password})
+  register({username, password}) {
+    return request(URL.REGISTER, 'POST', { username, password })
   },
-  //登录方法
-  login({username,password}){
-    return request(URL.LOGIN,'POST',{username,password})
+
+  login({username, password}) {
+    return request(URL.LOGIN, 'POST', { username, password })
   },
-  //注销方法
+
   logout() {
     return request(URL.LOGOUT)
   },
-  //登录状态
-  getInfo(){
+
+  getInfo() {
     return request(URL.GET_INFO)
   }
 }
